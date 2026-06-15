d4d9d8a3d7792e6521f3f8bd4d27387f70641956
Ravi Solanki <ravisolanki@Ravis-MacBook-Air.local>
2026-06-15 20:47:45 +0100

    Initial commit to repo

--- obj/Debug/net10.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86831863daccaf4f8d33f5018db37ddbc272c84c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
